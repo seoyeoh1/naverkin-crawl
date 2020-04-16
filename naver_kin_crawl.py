ac86df927b2e4aad5939b758c19eec7b06234d25
seoyeoh1 <63811369+seoyeoh1@users.noreply.github.com>
2020-04-17 03:23:41 +0900

    Update and rename naver_kin_crawl (1).py to naver_kin_crawl.py

--- naver_kin_crawl.py
+++ naver_kin_crawl.py
@@ -1,9 +1,6 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[178]:
-
-
 import pandas as pd
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
@@ -15,27 +12,15 @@ import requests
 from selenium.common.exceptions import UnexpectedAlertPresentException
 from selenium.common.exceptions import NoSuchElementException
 
-
-# In[ ]:
-
-
-
-
-
-# In[43]:
-
-
 def search_jisikin(query):
+    """지식인 검색"""
     elem_search = driver.find_element_by_class_name("search_input")
     elem_search_input = driver.find_element_by_class_name("search_btn")
     elem_search.send_keys(query)
     elem_search_input.click()
 
-
-# In[117]:
-
-
 def set_span(from_date, to_date):
+    """지식인에서 키워드 검색 이후 검색 결과 기간 설정"""
     select_span = driver.find_element_by_class_name("selectbox-box")
     span_input = driver.find_element_by_class_name("selectbox-notclose _nclicks:kin.dateinput selectbox-item selectbox-item-selected selectbox-item-over")
     elem_from_date = driver.find_element_by_id("sel_from_date")
@@ -49,31 +34,24 @@ def set_span(from_date, to_date):
     actions.send_keys(to_date).perform()
     actions.send_keys(Keys.Return).perform()
 
-
-# In[223]:
-
-
-driver = webdriver.Chrome('/Users/SeoyeonHong/Downloads/chromedriver')
-
+driver = webdriver.Chrome('/chromedriver')
 urls = []
 
+# 지식인 검색 결과 URL 주소 추출
 # 의사 답변
 def get_urls_doc(i):
-    driver.get("https://search.naver.com/search.naver?where=kin&kin_display=10&qt=&title=0&&answer=2&grade=0&            choice=0&sec=1&nso=so%3Ar%2Ca%3Aall%2Cp%3Afrom20191201to20200131&query=%EC%BD%94%EB%A1%9C%EB%82%98&c_id=&            c_name=&sm=tab_pge&kin_start={}".format (i))
+    driver.get("https://search.naver.com/search.naver?where=kin&kin_display=10&qt=&title=0&&answer=2&grade=0&choice=0&sec=1&nso=so%3Ar%2Ca%3Aall%2Cp%3Afrom20191201to20200131&query=%EC%BD%94%EB%A1%9C%EB%82%98&c_id=&c_name=&sm=tab_pge&kin_start={}".format(i))
     time.sleep(5)
     html = driver.page_source
     soup = bs(html, 'html.parser')
-
     questions = soup.select("dt.question")
     for question in questions[:]:
         url = question.find("a")["href"]
         urls.append(url)
-
 i = 1
 while i <= 12:
     get_urls_doc((i-1)*10 + 1)
     i += 1
-
 print(len(urls))
 
 # 약사 답변
@@ -85,30 +63,22 @@ questions = soup.select("dt.question")
 for question in questions[:]:
     url = question.find("a")["href"]
     urls.append(url)
-
 print(len(urls))
 
-with open('/Users/SeoyeonHong/Desktop/text_mining/jisikin_covid_201912_202001.txt', 'w') as f:
+with open('file_name.txt', 'w') as f: # 수집한 주소들을 .txt로 저장
     for item in urls:
         f.write("%s\n" % item)
 
 driver.close()
 
-
-# In[783]:
-
+# 지식인 Q&A 크롤링
 
 driver = webdriver.Chrome('/Users/SeoyeonHong/Downloads/chromedriver')
-
-with open("/Users/SeoyeonHong/Desktop/text_mining/jisikin_covid_201912_202001.txt") as sample:
+with open("file_name.txt") as sample:
     urls = []
     for line in sample:
         urls.append(line.rstrip())
     print(len(urls))
-
-
-# In[784]:
-
 
 q_title = []
 q_content = []
@@ -118,10 +88,6 @@ useful = []
 haha = []
 toobad = []
 likead = []
-
-
-# In[785]:
-
 
 def naver_kin_crawler(url):
     driver.get(url)
@@ -167,35 +133,12 @@ def naver_kin_crawler(url):
         likead_ = ''.join(filter(str.isdigit, likead_))
     likead.append(likead_)
 
-
-# In[793]:
-
-
 for url in urls[:]:
     naver_kin_crawler(url)
 
-
-# In[796]:
-
-
-data_ = zip(urls, q_title, q_content, answer, like, useful, haha, toobad, likead)
-df = pd.DataFrame(data = data_, columns=["url", "q_title", "q_content", "answer", "like", "useful", "haha", "toobad", "likead"])
-
-
-# In[801]:
-
-
-df.to_csv("/Users/SeoyeonHong/Desktop/text_mining/covid_naverkin_201912202001.csv")
-
-
-# In[808]:
-
-
 driver.close()
 
-
-# In[ ]:
-
-
-
-
+# 수집한 데이터를 .csv로 저장
+data_ = zip(urls, q_title, q_content, answer, like, useful, haha, toobad, likead)
+df = pd.DataFrame(data = data_, columns=["url", "q_title", "q_content", "answer", "like", "useful", "haha", "toobad", "likead"])
+df.to_csv("file_name.csv")
